396cf3b5446f1b04d342fcf7dc6c5f8d42339384
Ethan Valentine <evalentine12345@gmail.com>
2021-04-10 15:18:06 -0400

    work pls

--- scraper.js
+++ scraper.js
@@ -1,1 +1,17 @@
 const puppeteer = require("puppeteer");
+console.log("test");
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: false
+    });
+    const page = (await browser.pages())[0];
+    await page.goto(
+        "https://captcha.com/demos/features/captcha-demo.aspx",
+        { waitUntil: "load" }
+    );
+    await page.waitForSelector("#demoCaptcha_CaptchaImage");
+    const elementHandler = await page.$("#demoCaptcha_CaptchaImage");
+    const base64String = await elementHandler.screenshot({ encoding: "base64" });
+    console.log(base64String);
+})();
